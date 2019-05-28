dcb0c541881f30b7134cff2bf7386f1091339e70
github username <github email address>
2019-05-28 15:09:42 -0700

    worked on User form. Form now controlled. Liftedstates to App to allow form control. Form functional for User but nothing else.

--- src/components/Content.js
+++ src/components/Content.js
@@ -11,54 +11,13 @@ import Honors from './Honors'
 import Links from './Links'
 import { Grid } from 'semantic-ui-react'
 
-const apiURL = 'http://localhost:3000/api/v1/'
-
-const DEFAULT_STATE = {
-  jobs: [],
-  githubs: [],
-  interests: [],
-  skills: [],
-  honors: [],
-  links: [],
-  users: [],
-  currentUser: {},
-  editing: true
-}
-let keys = Object.keys(DEFAULT_STATE)
-let anchors = keys.slice(0, keys.length-3)
-// used to automate fetch -- users & currentUser are created  
-// together, and editing is a state, so they are excluded (length-3). 
-// Add any other resources before the final 3 in the list. 
-
 class Content extends Component {
   constructor() {
     super()
-    this.state = DEFAULT_STATE
   }
 
-  componentDidMount() {
-    //automated fetch
-    anchors.forEach( a => {
-      fetch( apiURL + a )
-      .then( res => res.json() )
-      .then( json => this.setState({[a]: json}))
-    })
-    //special fetch for users
-    fetch( apiURL + 'users')
-    .then( res => res.json() )
-    .then( users => {
-      this.setState({users})
-      this.setState({currentUser: users[0]})
-    })
-  }
-
-  getContent = (content) => {
-    console.log(content)
-    this.props.handleEdit(content)
-  }
 
   render() {
-    // TO DO: set up getContent and handleEdit for everything like it is in Skills
     return (
       <Grid columns="equal">
 
@@ -70,45 +29,45 @@ class Content extends Component {
 
         <Grid.Row key="name" id="name">
           <Grid.Column>
-            <NamePicIntro user={this.state.currentUser} editing={this.state.editing}
-              getContent={this.getContent} />
+            <NamePicIntro user={this.props.currentUser} editing={this.props.editing}
+              startEdit={this.startEdit} />
           </Grid.Column>
         </Grid.Row>
 
         <Grid.Row key="about" id="about">
           <Grid.Column>
-            <AboutMe user={this.state.currentUser} editing={this.state.editing}
-               getContent={this.getContent}
+            <AboutMe user={this.props.currentUser} editing={this.props.editing}
+               startEdit={this.props.startEdit}
             />
           </Grid.Column>
         </Grid.Row>
 
         <Grid.Row key="skills" id="skills">
           <Grid.Column>
-            <Skills skills={this.state.skills} editing={this.state.editing} 
-              getContent={this.getContent}
+            <Skills skills={this.props.skills} editing={this.props.editing}
+              startEdit={this.startEdit}
             />
           </Grid.Column>
         </Grid.Row>
 
         <Grid.Row key="jobs" id="jobs">
           <Grid.Column>
-            <Jobs jobs={this.state.jobs} editing={this.state.editing} 
-              getContent={this.getContent}/>
+            <Jobs jobs={this.props.jobs} editing={this.props.editing}
+              startEdit={this.startEdit}/>
           </Grid.Column>
         </Grid.Row>
 
         <Grid.Row key="github" id="github">
           <Grid.Column>
-            <Githubs githubs={this.state.githubs} editing={this.state.editing} 
-              getContent={this.getContent}/>
+            <Githubs githubs={this.props.githubs} editing={this.props.editing}
+              startEdit={this.startEdit}/>
           </Grid.Column>
         </Grid.Row>
 
         <Grid.Row key="contact" id="contact">
           <Grid.Column>
-            <Contact user={this.state.currentUser} editing={this.state.editing} 
-              getContent={this.getContent}/>
+            <Contact user={this.props.currentUser} editing={this.props.editing}
+              startEdit={this.startEdit}/>
           </Grid.Column>
         </Grid.Row>
 
@@ -121,26 +80,26 @@ export default Content;
 
 
 // visible: [
-//   <Nav user={this.state.currentUser} />, 
-//   <NamePicIntro user={this.state.currentUser} />,
-//   <AboutMe user={this.state.currentUser} />, 
-//   <Jobs jobs={this.state.jobs} />, 
-//   <Githubs githubs={this.state.githubs} />, 
-//   <Interests interests={this.state.interests}/>, 
-//   <Skills skills={this.state.skills} />, 
-//   <Honors honors={this.state.honors} />, 
-//   <Links links={this.state.links} />,
-//   <Contact user={this.state.currentUser} />,
-// ]  
+//   <Nav user={this.props.currentUser} />,
+//   <NamePicIntro user={this.props.currentUser} />,
+//   <AboutMe user={this.props.currentUser} />,
+//   <Jobs jobs={this.props.jobs} />,
+//   <Githubs githubs={this.props.githubs} />,
+//   <Interests interests={this.props.interests}/>,
+//   <Skills skills={this.props.skills} />,
+//   <Honors honors={this.props.honors} />,
+//   <Links links={this.props.links} />,
+//   <Contact user={this.props.currentUser} />,
+// ]
 
 // const SECTIONS = [
 //   {nav:  [<Nav openSidebar={this.props.openSidebar}/>]},
-//   {name: [<NamePicIntro user={this.state.currentUser}/>]},
-//   {about:[<AboutMe user={this.state.currentUser} />,
-//           <Skills skills={this.state.skills} />]},
-//   {jobs: [<Jobs jobs={this.state.jobs} />]},
-//   {githubs: [<Githubs githubs={this.state.githubs}/>]},
-//   {contact: [<Contact user={this.state.currentUser}/>]}
+//   {name: [<NamePicIntro user={this.props.currentUser}/>]},
+//   {about:[<AboutMe user={this.props.currentUser} />,
+//           <Skills skills={this.props.skills} />]},
+//   {jobs: [<Jobs jobs={this.props.jobs} />]},
+//   {githubs: [<Githubs githubs={this.props.githubs}/>]},
+//   {contact: [<Contact user={this.props.currentUser}/>]}
 // ]
 
 // {SECTIONS.map( section => {
